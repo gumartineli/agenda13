d66e66ca025c389b7cb4524b16a0c8c30da73692
gusta <gusta@MARTINELI>
2024-11-13 20:06:04 -0300

    Adicionar arquivos de projeto.

---
+++ agenda13/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace agenda13
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
